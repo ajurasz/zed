a9d14a6c77f5aa8143399813b7dc0504b34ab81e
Henryk Konsek <hekonsek@gmail.com>
2014-11-01 23:16:21 +0100

    Added Camel context.

--- service/jsoncrud/mongo/src/main/java/zed/service/jsoncrud/mongo/MongoJsonCrudServiceConfiguration.java
+++ service/jsoncrud/mongo/src/main/java/zed/service/jsoncrud/mongo/MongoJsonCrudServiceConfiguration.java
@@ -1,9 +1,20 @@
 package zed.service.jsoncrud.mongo;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 public class MongoJsonCrudServiceConfiguration {
+
+    // TODO Migrate to camel-spring-boot when Camel 2.15.0 is out -
+    // http://camel.apache.org/spring-boot.html
+    @Bean
+    CamelContext camelContext() {
+        return new SpringCamelContext();
+    }
+
 }
