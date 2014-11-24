d50894090d93746800d20209eaf2528cf97854a9
Henryk Konsek <hekonsek@gmail.com>
2014-11-24 17:16:10 +0100

    Refactoring.

--- quickstarts/fuse/61/blueprint/camel/dozer/src/test/java/zed/quickstarts/fuse61/blueprint/camel/dozer/MicoserviceTest.java
+++ quickstarts/fuse/61/blueprint/camel/dozer/src/test/java/zed/quickstarts/fuse61/blueprint/camel/dozer/MicoserviceTest.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.Callable;
 
@@ -15,21 +14,19 @@ public abstract class MicoserviceTest extends Assert {
 
     public static String waitForHttpQuery(final String url) {
         try {
-            final URL u = new URL(url);
+            final URL urlWrapper = new URL(url);
             await().atMost(60, SECONDS).until(new Callable<Boolean>() {
                 @Override
                 public Boolean call() throws Exception {
                     try {
-                        u.openStream();
+                        urlWrapper.openStream();
                         return true;
                     } catch (Exception e) {
                         return false;
                     }
                 }
             });
-            return IOUtils.toString(u).trim();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            return IOUtils.toString(urlWrapper).trim();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
