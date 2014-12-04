6b6be6e131fe94de6ae6969f413ad1268fb9ea90
Henryk Konsek <hekonsek@gmail.com>
2014-12-04 13:51:51 +0100

    Added JUnit level Docker container management.

--- quickstarts/fuse/61/blueprint/camel/dozer/src/test/java/zed/quickstarts/fuse61/blueprint/camel/dozer/FuseDozerIT.java
+++ quickstarts/fuse/61/blueprint/camel/dozer/src/test/java/zed/quickstarts/fuse61/blueprint/camel/dozer/FuseDozerIT.java
@@ -1,8 +1,36 @@
 package zed.quickstarts.fuse61.blueprint.camel.dozer;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FuseDozerIT extends MicoserviceTest {
+
+    static DockerClient docker;
+    static String containerId;
+
+    String xxx;
+
+    @BeforeClass
+    public static void beforeClass() {
+        DockerClientConfig config = DockerClientConfig.createDefaultConfigBuilder()
+                .withVersion("1.14").build();
+        docker = DockerClientBuilder.getInstance(config).build();
+
+        containerId = docker.createContainerCmd("com.github.zed-platform-zed-quickstarts-fuse-61-blueprint-camel-dozer").exec().getId();
+        docker.startContainerCmd(containerId).withPortBindings(new PortBinding(new Ports.Binding(18081), new ExposedPort(18081)), new PortBinding(new Ports.Binding(18082), new ExposedPort(18082))).exec();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        docker.stopContainerCmd(containerId).exec();
+    }
 
     @Test
     public void shouldConvertPojo() {
