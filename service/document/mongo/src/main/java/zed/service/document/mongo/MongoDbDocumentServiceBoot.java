947943c0c05f4051460f4cb86b3bdc4a1ab9772a
Henryk Konsek <hekonsek@gmail.com>
2014-12-08 12:45:23 +0100

    Refactoring.

--- service/document/mongo/src/main/java/zed/service/document/mongo/MongoDbDocumentServiceBoot.java
+++ service/document/mongo/src/main/java/zed/service/document/mongo/MongoDbDocumentServiceBoot.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class MongoDbDocumentServiceBoot extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         new SpringApplication(MongoDbDocumentServiceConfiguration.class).run(args);
     }
 
