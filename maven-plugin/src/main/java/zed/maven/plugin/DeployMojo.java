f66fd2bd24264d90e39cf6a54fafeaf8bf5b5ebd
Henryk Konsek <hekonsek@gmail.com>
2014-12-19 21:11:38 +0100

    Refactoring.

--- maven-plugin/src/main/java/zed/maven/plugin/DeployMojo.java
+++ maven-plugin/src/main/java/zed/maven/plugin/DeployMojo.java
@@ -1,44 +1,22 @@
 package zed.maven.plugin;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+import static org.apache.maven.plugins.annotations.LifecyclePhase.PROCESS_SOURCES;
 import static zed.utils.Mavens.artifactVersion;
 
-@Mojo(name = "deploy", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+@Mojo(name = "deploy", defaultPhase = PROCESS_SOURCES)
 public class DeployMojo extends AbstractMojo {
-    /**
-     * Location of the file.
-     */
-    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
-    private File outputDirectory;
 
     @Parameter(defaultValue = "${project}", required = true)
     private MavenProject project;
@@ -71,30 +49,6 @@ public class DeployMojo extends AbstractMojo {
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        File f = outputDirectory;
-
-        if (!f.exists()) {
-            f.mkdirs();
-        }
-
-        File touch = new File(f, "touch.txt");
-
-        FileWriter w = null;
-        try {
-            w = new FileWriter(touch);
-
-            w.write("touch.txt");
-        } catch (IOException e) {
-            throw new MojoExecutionException("Error creating file " + touch, e);
-        } finally {
-            if (w != null) {
-                try {
-                    w.close();
-                } catch (IOException e) {
-                    // ignore
-                }
-            }
-        }
     }
+
 }
