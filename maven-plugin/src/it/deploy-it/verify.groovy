7ec7df80431f22b85d9a3def69c6d195e2b405f3
Henryk Konsek <hekonsek@gmail.com>
2014-12-29 11:17:19 +0100

    Fixed broken test.

--- maven-plugin/src/it/deploy-it/verify.groovy
+++ maven-plugin/src/it/deploy-it/verify.groovy
@@ -1,2 +1,2 @@
-def files = new File(System.getProperty("user.home") + "/.zed/deploy").list() as List
+def files = new File(System.getProperty("user.home") + "/.zed/deploy/default").list() as List
 assert files.contains("guava-18.0.jar")
