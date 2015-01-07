36e2fb08531caa4f7667397df124dd681ee9f27e
Henryk Konsek <hekonsek@gmail.com>
2015-01-07 21:04:01 +0100

    Improved Maven Plugin tests.

--- maven-plugin/src/it/deploy-it/verify.groovy
+++ maven-plugin/src/it/deploy-it/verify.groovy
@@ -1,2 +1,6 @@
-def filesInMyWorkspace = new File(System.getProperty("user.home") + "/.zed/deploy/myworkspace").list() as List
+import java.nio.file.Paths
+
+import static org.apache.commons.lang3.SystemUtils.USER_HOME
+
+def filesInMyWorkspace = Paths.get(USER_HOME, ".zed", "deploy", "myworkspace").toFile().list() as List
 assert filesInMyWorkspace.contains("guava-18.0.jar")
