18c57466fdb6ca1042deb669c9f6fe247737128f
Henryk Konsek <hekonsek@gmail.com>
2015-01-28 17:12:36 +0100

    [Panel] Added DocumentService connectivity POC.

--- panel/src/main/webapp/scripts/app/main/main.controller.js
+++ panel/src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('zedpanelApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, $http, Principal) {
         Principal.identity().then(function (account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+
+            $scope.details = function (collection) {
+                $http.get('http://localhost:15001/api/document/count/' + collection).
+                    success(function (data, status, headers, config) {
+                        $scope.collectionCount = data;
+                        $scope.flash = null
+                    }).
+                    error(function (data, status, headers, config) {
+                        $scope.flash = 'Can\'t connect to the Document Service.'
+                    });
+            };
+
+            var collections = ['foo', 'bar'];
+            $scope.collections = collections;
         });
     });
