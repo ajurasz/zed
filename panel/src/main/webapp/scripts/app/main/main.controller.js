cc19ec54117dcb14df59091bfd58c68a64a41525
Henryk Konsek <hekonsek@gmail.com>
2015-02-06 22:48:28 +0100

    Added MongoDB service handler to ThingsCloud landing page.

--- panel/src/main/webapp/scripts/app/main/main.controller.js
+++ panel/src/main/webapp/scripts/app/main/main.controller.js
@@ -6,19 +6,43 @@ angular.module('zedpanelApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
 
-            $http.get('http://localhost:8080/jolokia/exec/zed:name=zedShell/invokeCommand/deploy_list').
-                success(function (data, status, headers, config) {
-                    var deployListHeaderLinesCount = 2;
-                    $scope.deployablesCount = data.value.length - deployListHeaderLinesCount;
-                    $scope.flash = null
-                }).
-                error(function (data, status, headers, config) {
-                    $scope.flash = 'Can\'t connect to the ThingsCloud shell.'
-                });
+            $scope.selection = [];
+            $scope.availableServices = [
+                ["mongodb", "MongoDB"],
+                ["attachmentService", "Binary Attachments Service"]
+            ];
 
-            var staticAvailableServices = {};
-            staticAvailableServices["mongodb"] = "MongoDB";
-            staticAvailableServices["attachmentService"] = "Binary Attachments Service";
-            $scope.availableServices = staticAvailableServices
+            shellCommand($http, 'deploy_list', function (data) {
+                var deployListHeaderLinesCount = 2;
+                $scope.deployablesCount = data.value.length - deployListHeaderLinesCount;
+                $scope.flash = null
+            }, function () {
+                $scope.flash = 'Can\'t connect to the ThingsCloud shell.'
+            });
+
+            $scope.selectService = function toggleSelection(selectedValue) {
+                var idx = $scope.selection.indexOf(selectedValue);
+                if (idx > -1) {
+                    $scope.selection.splice(idx, 1);
+                } else {
+                    $scope.selection.push(selectedValue);
+                }
+            };
+
+            $scope.initialize = function () {
+                $scope.flash = 'Initializing ThingsCloud. Please wait...';
+                if ($scope.selection.indexOf("mongodb") > -1) {
+                    shellCommand($http, 'deploy mongodb:docker', function (data) {
+                        shellCommand($http, 'deploy_start_all', function (data) {
+                            $scope.deployablesCount = 1;
+                            $scope.flash = null;
+                        }, function () {
+                            $scope.flash = 'Can\'t connect to the ThingsCloud shell.'
+                        });
+                    }, function () {
+                        $scope.flash = 'Can\'t connect to the ThingsCloud shell.'
+                    });
+                }
+            }
         });
     });
