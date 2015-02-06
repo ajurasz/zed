c11ddc73bed944b3ba22e924cbf1b2de00c5dc69
Henryk Konsek <hekonsek@gmail.com>
2015-02-06 14:47:53 +0100

    Added ThingsCloud landing page.

--- panel/src/main/webapp/scripts/app/main/main.controller.js
+++ panel/src/main/webapp/scripts/app/main/main.controller.js
@@ -6,18 +6,19 @@ angular.module('zedpanelApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
 
-            $scope.details = function (collection) {
-                $http.get('http://localhost:15001/api/document/count/' + collection).
-                    success(function (data, status, headers, config) {
-                        $scope.collectionCount = data;
-                        $scope.flash = null
-                    }).
-                    error(function (data, status, headers, config) {
-                        $scope.flash = 'Can\'t connect to the Document Service.'
-                    });
-            };
+            $http.get('http://localhost:8080/jolokia/exec/zed:name=zedShell/invokeCommand/deploy_list').
+                success(function (data, status, headers, config) {
+                    var deployListHeaderLinesCount = 2;
+                    $scope.deployablesCount = data.value.length - deployListHeaderLinesCount;
+                    $scope.flash = null
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.flash = 'Can\'t connect to the ThingsCloud shell.'
+                });
 
-            var collections = ['foo', 'bar'];
-            $scope.collections = collections;
+            var staticAvailableServices = {};
+            staticAvailableServices["mongodb"] = "MongoDB";
+            staticAvailableServices["attachmentService"] = "Binary Attachments Service";
+            $scope.availableServices = staticAvailableServices
         });
     });
