5ab2b7995c5297f7e4eae23481232dbfbab65d75
Henryk Konsek <hekonsek@gmail.com>
2015-02-08 22:40:43 +0100

    Fixed Docker URL.

--- thingscloud.sh
+++ thingscloud.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -e DOCKER_HOST=tcp://172.16.42.1:2375 -p 8080:8080 -p 15005:15005 -it hekonsek/zedpanel:0.0.17
+docker run -e DOCKERHOST=tcp://172.16.42.1:2375 -p 8080:8080 -p 15005:15005 -it hekonsek/zedpanel:0.0.17
